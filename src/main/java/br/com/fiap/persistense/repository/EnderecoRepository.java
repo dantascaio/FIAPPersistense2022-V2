17eb9ae6deed31c0994644ce24c5906aa8546ad5
Vinicius Rodrigues Tonha <vinirt1@gmail.com>
2022-04-24 22:04:24 -0300

    Exclui CRUD endereco

--- src/main/java/br/com/fiap/persistense/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.persistense.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.persistense.model.Endereco;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {}
